7f9b76448209e3aec39885be554d3a4429bb6a96
José Souza <joseedsouza.dev@gmail.com>
2024-08-17 18:37:37 -0300

    initial commit

---
+++ src/main/java/com/handleservice/handleworkservice/HandleWorkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.handleservice.handleworkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleWorkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleWorkServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handleservice/handleworkservice/HandleWorkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handleservice.handleworkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleWorkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
