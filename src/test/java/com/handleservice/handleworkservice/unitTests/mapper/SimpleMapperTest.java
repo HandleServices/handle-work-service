ed6f7b5b6038dd9c4cbfe1d0274e758b335abfd2
José Souza <joseedsouza.dev@gmail.com>
2024-08-21 19:46:57 -0300

    test: improve SimpleMapperTest

--- src/test/java/com/handleservice/handleworkservice/unitTests/mapper/SimpleMapperTest.java
+++ src/test/java/com/handleservice/handleworkservice/unitTests/mapper/SimpleMapperTest.java
@@ -5,42 +5,63 @@ import com.handleservice.handleworkservice.mapper.SimpleEntityMapper;
 import com.handleservice.handleworkservice.utils.Pair;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+// Example Entity class for testing
 @Data
 @NoArgsConstructor
-class TestModelClass {
+class TestEntity {
     private String name;
     private double value;
 }
 
+// Example DTO class for testing
 @Data
 @NoArgsConstructor
-class TestModelClass2 {
+class TestDTO {
     private String name;
     private double value;
     private int value2;
 }
 
 
-public class SimpleMapperTest extends BaseMapperTest<TestModelClass, TestModelClass2> {
+public class SimpleMapperTest extends BaseMapperTest<TestEntity, TestDTO> {
 
     protected SimpleMapperTest() {
-        super(new SimpleEntityMapper<>(TestModelClass.class, TestModelClass2.class));
+        super(new SimpleEntityMapper<>(TestEntity.class, TestDTO.class));
     }
 
     @SuppressWarnings("all")
     @Override
-    protected Pair<TestModelClass, TestModelClass2> getMatchingValues() {
+    protected Pair<TestEntity, TestDTO> getMatchingValues() {
         String name = "teste";
         double value = 1.0;
-        TestModelClass testModelClass = new TestModelClass();
-        testModelClass.setName(name);
-        testModelClass.setValue(value);
+        TestEntity testEntity = new TestEntity();
+        testEntity.setName(name);
+        testEntity.setValue(value);
 
-        TestModelClass2 testModelClass2 = new TestModelClass2();
-        testModelClass2.setName(name);
-        testModelClass2.setValue(value);
+        TestDTO testDTO = new TestDTO();
+        testDTO.setName(name);
+        testDTO.setValue(value);
 
-        return new Pair<>(testModelClass, testModelClass2);
+        return new Pair<>(testEntity, testDTO);
+    }
+
+    // Example record class for testing
+    record TestRecord(String name, int value) {
+    }
+
+    @Test
+    void shouldThrowExceptionForRecordClass() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SimpleEntityMapper<>(TestRecord.class, TestDTO.class));
+
+    }
+
+    @Test
+    void shouldNotThrowExceptionForNonRecordClass() {
+        Assertions.assertDoesNotThrow(() -> {
+            new SimpleEntityMapper<>(TestEntity.class, TestDTO.class);
+        });
     }
 }
