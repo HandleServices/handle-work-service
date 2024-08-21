0bb242d4bca0227a27a2fe126dbe1c97c75e9ba6
José Souza <joseedsouza.dev@gmail.com>
2024-08-21 19:46:40 -0300

    chore: make SimpleEntityMapper throws Exception if used with a record instead of a class

--- src/main/java/com/handleservice/handleworkservice/mapper/SimpleEntityMapper.java
+++ src/main/java/com/handleservice/handleworkservice/mapper/SimpleEntityMapper.java
@@ -1,6 +1,7 @@
 package com.handleservice.handleworkservice.mapper;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.util.Assert;
 
 public class SimpleEntityMapper<T, U> implements EntityMapper<T, U> {
 
@@ -9,7 +10,9 @@ public class SimpleEntityMapper<T, U> implements EntityMapper<T, U> {
 
     protected final ModelMapper modelMapper = new ModelMapper();
 
-    public SimpleEntityMapper(Class<T> entityClass, Class<U> dtoClass) {
+    public SimpleEntityMapper(Class<T> entityClass, Class<U> dtoClass) throws IllegalArgumentException {
+        Assert.isTrue(!entityClass.isRecord(), "entityClass cannot be a record");
+        Assert.isTrue(!dtoClass.isRecord(), "dtoClass cannot be a record");
         this.dtoClass = dtoClass;
         this.entityClass = entityClass;
     }
