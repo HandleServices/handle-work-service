61f433ac4749733e3c4659db1e0beb91a892cfa1
José Souza <joseedsouza.dev@gmail.com>
2024-08-23 16:11:01 -0300

    chore: improve WorkRepository with methods counts on workerId

--- src/main/java/com/handleservice/handleworkservice/repository/WorkRepository.java
+++ src/main/java/com/handleservice/handleworkservice/repository/WorkRepository.java
@@ -2,6 +2,26 @@ package com.handleservice.handleworkservice.repository;
 
 import com.handleservice.handleworkservice.model.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface WorkRepository extends JpaRepository<Work, Long> {
+    Optional<Work> findByIdAndWorkerId(Long id, UUID workerId);
+
+    List<Work> findAllByWorkerId(UUID workerId);
+
+    @Modifying
+    @Query("UPDATE Work w SET w.name = :#{#newWork.name}, w.description = :#{#newWork.description}, w.enable = :#{#newWork.enable} WHERE w.id = :id AND w.workerId = :workerId ")
+    Work updateWorkByIdAndWorkerId(
+            @Param("id") Long id,
+            @Param("workerId") UUID workerId,
+            @Param("newWork") Work newWork
+    );
+
+    Boolean existsByIdAndWorkerId(Long id, UUID workerId);
 }
