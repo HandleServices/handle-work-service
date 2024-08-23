2a1c18354006f18cc4551b80892d199a93166375
José Souza <joseedsouza.dev@gmail.com>
2024-08-23 16:09:50 -0300

    refactor: rename EntityNotFoundError.java to EntityNotFoundException.java

--- src/main/java/com/handleservice/handleworkservice/exception/EntityNotFoundError.java
+++
@@ -1,7 +0,0 @@
-package com.handleservice.handleworkservice.exception;
-
-public class EntityNotFoundError extends RuntimeException {
-    public EntityNotFoundError(String message) {
-        super(message);
-    }
-}
