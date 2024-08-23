f39b6f2787ab62e791ac0ed80ac36fd7cee2e6fb
José Souza <joseedsouza.dev@gmail.com>
2024-08-23 18:36:05 -0300

    test: fix WorkRepositoryTest testing dependencies

--- src/test/java/com/handleservice/handleworkservice/integrationTests/WorkRepositoryTest.java
+++ src/test/java/com/handleservice/handleworkservice/integrationTests/WorkRepositoryTest.java
@@ -15,15 +15,12 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class WorkRepositoryTest {
 
-    // Crie o container PostgreSQL com a mesma versão usada no Docker Compose
-    private static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16.4-alpine");
+    public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:16.4-alpine");
 
     @BeforeAll
     static void setUp() {
-        // Inicie o container PostgreSQL
         postgresContainer.start();
 
-        // Defina as propriedades do datasource com base no container iniciado
         System.setProperty("spring.datasource.url", postgresContainer.getJdbcUrl());
         System.setProperty("spring.datasource.username", postgresContainer.getUsername());
         System.setProperty("spring.datasource.password", postgresContainer.getPassword());
