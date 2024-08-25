8b9996218b0ae47d0934222ac17e974e828cbd0d
José Souza <joseedsouza.dev@gmail.com>
2024-08-25 11:30:53 -0300

    docs: add swagger documentation in WorkController

--- src/main/java/com/handleservice/handleworkservice/controller/WorkController.java
+++ src/main/java/com/handleservice/handleworkservice/controller/WorkController.java
@@ -6,6 +6,8 @@ import com.handleservice.handleworkservice.mapper.work.WorkMapper;
 import com.handleservice.handleworkservice.model.Work;
 import com.handleservice.handleworkservice.service.jwt.IJwtService;
 import com.handleservice.handleworkservice.service.work.IWorkService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/work")
+@Tag(name = "Work", description = "API for managing work entities")
 public class WorkController {
 
     private final IJwtService _jwtService;
@@ -27,15 +30,17 @@ public class WorkController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all works", description = "Retrieve all works for a specific worker")
     public List<WorkDTO> getAllWorks() {
         UUID workerId = UUID.fromString("3d92e6fa-964c-4fb7-b915-a5c897e02dcb");
         return _workService.findAll(workerId).stream().map(_workMapper::toDTO).toList();
     }
 
     @PostMapping
-    public WorkDTO insert(@RequestBody CreateWorkDTO createWorkDTO){
+    @Operation(summary = "Insert a new work", description = "Insert a new work entity for a specific worker")
+    public WorkDTO insert(@RequestBody CreateWorkDTO createWorkDTO) {
         UUID workerId = UUID.fromString("3d92e6fa-964c-4fb7-b915-a5c897e02dcb");
-        Work w =  _workService.insert(workerId, createWorkDTO);
+        Work w = _workService.insert(workerId, createWorkDTO);
         return _workMapper.toDTO(w);
     }
 }
