ef06db1a4112d0a44264846385ec8a3912b241bf
José Souza <joseedsouza.dev@gmail.com>
2024-08-27 20:33:26 -0300

    chore: add field validation

--- src/main/java/com/handleservice/handleworkservice/dto/work/CreateWorkDTO.java
+++ src/main/java/com/handleservice/handleworkservice/dto/work/CreateWorkDTO.java
@@ -1,13 +1,24 @@
 package com.handleservice.handleworkservice.dto.work;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
 public record CreateWorkDTO(
+        @NotBlank
+        @Size(min = 2, max = 100, message = "The name is too short or too big")
         String name,
-        @Nullable String description,
+
+        @NotBlank
+        @Nullable
+        String description,
+
+        @Pattern(regexp = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", message = "The estimatedTime must match hh:mm")
         String estimatedTime,
+
+        @Max(value=99999999, message = "The value must not exceed 99999999")
+        @Min(value = 0, message = "The value must not be negative")
         BigDecimal value
 ) {
 }
--- src/main/java/com/handleservice/handleworkservice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/handleservice/handleworkservice/exception/handler/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.handleservice.handleworkservice.exception.handler;
 import com.handleservice.handleworkservice.exception.custom.DomainException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,4 +22,22 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse(ex.getHttpStatus(), ex.getMessage());
         return new ResponseEntity<>(errorResponse, status);
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            if (error instanceof FieldError) {
+                String fieldErrorName = ((FieldError) error).getField();
+                String errorMessage = error.getDefaultMessage();
+                errors.put(fieldErrorName, errorMessage);
+            }
+        });
+
+        return errors;
+    }
+
 }
