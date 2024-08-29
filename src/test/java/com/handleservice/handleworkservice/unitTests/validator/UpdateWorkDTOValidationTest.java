f1f58514cc716dae55b4dab35a29f9a4bdaaa990
José Souza <joseedsouza.dev@gmail.com>
2024-08-29 00:40:22 -0300

    test: add validation tests

---
+++ src/test/java/com/handleservice/handleworkservice/unitTests/validator/UpdateWorkDTOValidationTest.java
@@ -0,0 +1,35 @@
+package com.handleservice.handleworkservice.unitTests.validator;
+
+import com.handleservice.handleworkservice.dto.work.UpdateWorkDTO;
+
+import java.math.BigDecimal;
+
+public class UpdateWorkDTOValidationTest extends BaseValidationTest<UpdateWorkDTO> {
+
+    @Override
+    protected UpdateWorkDTO getValidObject() {
+        return new UpdateWorkDTO(
+                "Valid name",
+                "Valid description",
+                BigDecimal.valueOf(1),
+                "12:30",
+                true
+        );
+    }
+
+    @Override
+    protected UpdateWorkDTO getInvalidObject() {
+        return new UpdateWorkDTO(
+                "", // Invalid name: too short
+                null,
+                BigDecimal.valueOf(100000000), // Invalid value: exceeds maximum
+                "25:61", // Invalid estimatedTime: does not match pattern
+                true
+        );
+    }
+
+    @Override
+    protected int getErrorsCount() {
+        return 3;
+    }
+}
